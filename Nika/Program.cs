ad5fa16d55d5e65aa33e3b1430cc1bdaab5e85bd
nikanairashvili3 <nikanadirashvili8@gmail.com>
2022-05-01 17:53:34 +0400

    first commit

---
+++ Nika/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.DarkBlue;
+            //Console.WriteLine("Nika NadiraSvili");
+            Console.ReadLine();
+        }
+    }
+}
